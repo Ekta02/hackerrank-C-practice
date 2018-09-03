9bb5b0767ce17af057f999798e7b57cb4a982ce4
Ekta02 <42929625+Ekta02@users.noreply.github.com>
2018-09-03 11:06:58 -0700

    New Commit

---
+++ 1D Arrays in C.c
@@ -0,0 +1,44 @@
+/*
+1D Arrays in C
+An array is a container object that holds a fixed number of values of a single type. To create an array in C,
+we can do int arr[n];. Here, arr, is a variable array which holds up to integers. The above array is a
+static array that has memory allocated at compile time. A dynamic array can be created in C, using the
+malloc function and the memory is allocated on the heap at runtime. To create an integer array, of
+size , int *arr = (int*)malloc(n * sizeof(int)) , where points to the base address of the array.
+In this challenge, you have to create an array of size dynamically, input the elements of the array, sum
+them and print the sum of the elements in a new line.
+Input Format
+The first line contains an integer, .
+The next line contains space-separated integers.
+Constraints
+Output Format
+Print in a single line the sum of the integers in the array.
+Sample Input 0
+6
+16 13 7 2 1 12
+Sample Output 0
+51
+Sample Input 1
+7
+1 13 15 20 12 13 2
+Sample Output 1
+76*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+int s[1000],n,a=0,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+    scanf(" %d",&s[i]);
+        a=a+s[i];
+    }
+    printf("%d ",a);
+    return 0;
+}
+
